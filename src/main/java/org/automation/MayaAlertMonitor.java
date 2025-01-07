60bd427d512f86f357a73118ecfd9bd9766ce4e7
Eyal Sooliman <soul3510@gmail.com>
2025-01-07 14:52:14 +0200

    Screen shot

--- src/main/java/org/automation/MayaAlertMonitor.java
+++ src/main/java/org/automation/MayaAlertMonitor.java
@@ -29,8 +29,6 @@ import com.twilio.type.PhoneNumber;
 import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import java.io.File;
 import org.apache.commons.io.FileUtils;
 
 public class MayaAlertMonitor {
@@ -50,136 +48,138 @@ public class MayaAlertMonitor {
         options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
 
         // Initialize WebDriver
-        WebDriver driver = new ChromeDriver(options);
+        try {
+            WebDriver driver = new ChromeDriver(options);
 
 
-        // Base URL
-        String baseUrl = "https://maya.tase.co.il/reports/company";
+            // Base URL
+            String baseUrl = "https://maya.tase.co.il/reports/company";
 
-        LocalDate todayUTC = LocalDate.now(ZoneOffset.UTC);
-        LocalDate yesterdayUTC = todayUTC.minusDays(1);
-        ZonedDateTime startOfDayUTC = yesterdayUTC.atStartOfDay(ZoneOffset.UTC);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-        String formattedDateFrom = startOfDayUTC.format(formatter);
+            LocalDate todayUTC = LocalDate.now(ZoneOffset.UTC);
+            LocalDate yesterdayUTC = todayUTC.minusDays(1);
+            ZonedDateTime startOfDayUTC = yesterdayUTC.atStartOfDay(ZoneOffset.UTC);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            String formattedDateFrom = startOfDayUTC.format(formatter);
+            String formattedDateTo = "2027-03-30T21:00:00.000Z";
 
+            // JSON parameters
+            Map<String, Object> queryParams = new HashMap<>();
+            queryParams.put("DateFrom", formattedDateFrom);
+            queryParams.put("DateTo", formattedDateTo);
+            queryParams.put("events", Arrays.asList(200, 100, 300, 400, 1100, 1400, 500, 1300, 600, 1600, 1200, 700, 1500, 900));
+            queryParams.put("subevents", Arrays.asList(
+                    201, 270, 271, 202, 203, 254, 210, 211, 212, 907, 219, 205, 206, 258, 251, 236, 255,
+                    240, 252, 256, 250, 227, 226, 231, 259, 229, 278, 101, 103, 104, 105, 106, 114, 113,
+                    233, 273, 222, 282, 109, 281, 220, 280, 228, 110, 111, 112, 283, 102, 322, 320, 310,
+                    316, 318, 317, 319, 326, 309, 324, 301, 302, 303, 304, 315, 327, 312, 323, 325, 313,
+                    401, 403, 405, 402, 404, 501, 502, 504, 2460, 204, 1102, 1103, 1104, 1105, 221, 1404,
+                    1401, 1402, 230, 223, 1406, 238, 1405, 215, 239, 216, 253, 245, 217, 214, 241, 249,
+                    243, 244, 246, 247, 248, 910, 235, 260, 225, 265, 915, 234, 207, 208, 209, 242, 2560,
+                    620, 605, 603, 601, 602, 621, 604, 606, 615, 613, 611, 612, 622, 614, 616, 218, 1690,
+                    1202, 1605, 1692, 1610, 1670, 1615, 1620, 1635, 1625, 1630, 1640, 1645, 1660, 1204,
+                    1203, 1680, 1650, 308, 224, 330, 904, 1201, 311, 305, 314, 307, 306, 707, 701, 702,
+                    703, 704, 213, 1501, 1502, 1503, 1504, 909, 908, 903, 901, 902, 911, 912, 914, 905, 906
+            ));
+            queryParams.put("Page", 1);
+            queryParams.put("IsBreakingAnnouncement", true);
 
-        // JSON parameters
-        Map<String, Object> queryParams = new HashMap<>();
-        queryParams.put("DateFrom", formattedDateFrom);
-        queryParams.put("DateTo", "2040-12-30T22:00:00.000Z");
-        queryParams.put("events", Arrays.asList(200, 100, 300, 400, 1100, 1400, 500, 600, 1200));
-        queryParams.put("subevents", Arrays.asList(
-                201, 270, 271, 202, 203, 254, 210, 211, 212, 907, 219, 205, 206, 258, 251, 236, 255,
-                240, 252, 256, 250, 227, 226, 231, 259, 229, 278, 101, 103, 104, 105, 106, 114, 113,
-                233, 273, 222, 282, 109, 281, 220, 280, 228, 110, 111, 112, 283, 102, 322, 320, 310,
-                316, 318, 317, 319, 326, 309, 324, 301, 302, 303, 304, 315, 327, 312, 323, 325, 313,
-                401, 403, 405, 402, 404, 501, 502, 504, 2460, 204, 1102, 1103, 1104, 1105, 221, 1404,
-                1401, 1402, 230, 223, 1406, 238, 1405, 215, 239, 216, 253, 245, 217, 214, 241, 249,
-                243, 244, 246, 247, 248, 910, 620, 605, 603, 601, 602, 621, 604, 606, 615, 613, 611,
-                612, 622, 614, 616, 308, 224, 330, 904, 1201, 311, 305, 314, 307, 306
-        ));
-        queryParams.put("IsBreakingAnnouncement", true);
+            // Initialize ObjectMapper for JSON processing
+            ObjectMapper mapper = new ObjectMapper();
 
-        // Initialize ObjectMapper for JSON processing
-        ObjectMapper mapper = new ObjectMapper();
+            // Convert query parameters to JSON string
+            String jsonParams = mapper.writeValueAsString(queryParams);
 
-        // Convert query parameters to JSON string
-        String jsonParams = mapper.writeValueAsString(queryParams);
+            // URL-encode the JSON string
+            String encodedParams = URLEncoder.encode(jsonParams, StandardCharsets.UTF_8);
 
-        // URL-encode the JSON string
-        String encodedParams = URLEncoder.encode(jsonParams, StandardCharsets.UTF_8);
-
-        // Construct full URL
-        String fullUrl = baseUrl + "?q=" + encodedParams;
-
-
-        System.out.println("Starting MayaAlertMonitor...");
-
-
-        System.out.println(fullUrl);
-
-        // Navigate to the URL
-        driver.get(fullUrl);
-
-        // Wait until the reports container is loaded
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"scrTo\"]/maya-report-actions/div[4]")));
+            // Construct full URL
+            String fullUrl = baseUrl + "?q=" + encodedParams;
 
 
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshot, new File("screenshot.png"));
-        System.out.println("Screenshot taken");
-
-        System.out.println("Page is up...");
-
-        // Find all report entries
-        List<WebElement> allDates = driver.findElements(By.xpath("//div[contains(@class,'feedItemDate hidden-xs')]"));
-        List<WebElement> allCampanies = driver.findElements(By.xpath("//h2[@class='ng-binding']"));
-        List<WebElement> allAnnouncements = driver.findElements(By.xpath("//a[contains(@class,'messageContent ng-binding')]"));
+            System.out.println("Starting MayaAlertMonitor...");
 
 
+            System.out.println(fullUrl);
 
-        System.out.println("All entries fetched...");
-        System.out.println("Number of dates: " + allDates.size());
-        System.out.println("Number of companies: " + allCampanies.size());
-        System.out.println("Number of announcements: " + allAnnouncements.size());
+            // Navigate to the URL
+            driver.get(fullUrl);
+
+            // Wait until the reports container is loaded
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"scrTo\"]/maya-report-actions/div[4]")));
 
 
+            System.out.println("Page is up...");
 
-        for (int i = 0; i < allDates.size(); i++) {
-            System.out.println("Date: " + allDates.get(i).getText());
-            System.out.println("Company: " + allCampanies.get(i).getText());
-            System.out.println("Message: " + allAnnouncements.get(i).getText());
-            if (i == 2) {
-                //Stop
-                break;
+            // Find all report entries
+            List<WebElement> allDates = driver.findElements(By.xpath("//div[contains(@class,'feedItemDate hidden-xs')]"));
+            List<WebElement> allCampanies = driver.findElements(By.xpath("//h2[@class='ng-binding']"));
+            List<WebElement> allAnnouncements = driver.findElements(By.xpath("//a[contains(@class,'messageContent ng-binding')]"));
+
+
+            System.out.println("All entries fetched...");
+            System.out.println("Number of dates: " + allDates.size());
+            System.out.println("Number of companies: " + allCampanies.size());
+            System.out.println("Number of announcements: " + allAnnouncements.size());
+
+
+            for (int i = 0; i < allDates.size(); i++) {
+                System.out.println("Date: " + allDates.get(i).getText());
+                System.out.println("Company: " + allCampanies.get(i).getText());
+                System.out.println("Message: " + allAnnouncements.get(i).getText());
+                if (i == 2) {
+                    //Stop
+                    break;
+                }
             }
-        }
 
 
-        String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-        String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
-        String FROM_WHATSAPP_NUMBER = "whatsapp:+14155238886"; // Twilio Sandbox WhatsApp number
-        String TO_WHATSAPP_NUMBER = "whatsapp:+972508266273";
+            String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+            String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+            String FROM_WHATSAPP_NUMBER = "whatsapp:+14155238886"; // Twilio Sandbox WhatsApp number
+            String TO_WHATSAPP_NUMBER = "whatsapp:+972508266273";
 
 
+            // Initialize Twilio
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        // Initialize Twilio
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-
-        // StringBuilder to compile the message
-        StringBuilder messageBody = new StringBuilder();
-        System.out.println("Trying to send whatsup message with: " + messageBody);
+            // StringBuilder to compile the message
+            StringBuilder messageBody = new StringBuilder();
+            System.out.println("Trying to send whatsup message with: " + messageBody);
 
 
-        for (int i = 0; i < allDates.size(); i++) {
-            messageBody.append(allDates.get(i).getText()).append("\n");
-            messageBody.append(allCampanies.get(i).getText()).append("\n");
-            messageBody.append(allAnnouncements.get(i).getText()).append("\n\n");
-            if (i == 2) {
-                //Stop
-                break;
+            for (int i = 0; i < allDates.size(); i++) {
+                messageBody.append(allDates.get(i).getText()).append("\n");
+                messageBody.append(allCampanies.get(i).getText()).append("\n");
+                messageBody.append(allAnnouncements.get(i).getText()).append("\n\n");
+                if (i == 2) {
+                    //Stop
+                    break;
+                }
             }
+
+            // Convert StringBuilder to String
+            String finalMessage = messageBody.toString();
+
+
+            // Send WhatsApp message via Twilio
+            Message message = Message.creator(
+                    new PhoneNumber(TO_WHATSAPP_NUMBER), // To number
+                    new PhoneNumber(FROM_WHATSAPP_NUMBER), // From number (Twilio Sandbox)
+                    finalMessage // Message body
+            ).create();
+
+            // Print the message SID for confirmation
+            System.out.println("WhatsApp Message sent with SID: " + message.getSid());
+
+
+            driver.close();
+            driver.quit();
+
+            System.out.println("Done. ");
+        } catch (Exception e) {
+            System.err.println("An error occurred during execution: " + e.getMessage());
+            e.printStackTrace();
         }
-
-        // Convert StringBuilder to String
-        String finalMessage = messageBody.toString();
-
-
-        // Send WhatsApp message via Twilio
-        Message message = Message.creator(
-                new PhoneNumber(TO_WHATSAPP_NUMBER), // To number
-                new PhoneNumber(FROM_WHATSAPP_NUMBER), // From number (Twilio Sandbox)
-                finalMessage // Message body
-        ).create();
-
-        // Print the message SID for confirmation
-        System.out.println("WhatsApp Message sent with SID: " + message.getSid());
-
-
-        driver.close();
-        driver.quit();
-
-        System.out.println("Done. ");
     }
 }
