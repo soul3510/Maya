88e22ace9a56ea4678f1eefa74b5f6b1b7255479
Eyal Sooliman <soul3510@gmail.com>
2025-01-08 09:59:05 +0200

    SMS

--- src/main/java/org/automation/MayaAlertMonitor.java
+++ src/main/java/org/automation/MayaAlertMonitor.java
@@ -140,7 +140,8 @@ public class MayaAlertMonitor {
     public static void sendWhatapp(String finalMessage) {
         String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
         String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
-        String FROM_WHATSAPP_NUMBER = "whatsapp:+12186950942"; // Twilio Sandbox WhatsApp number
+        String FROM_WHATSAPP_NUMBER = "whatsapp:+14155238886"; // Twilio Sandbox WhatsApp number
+//        String FROM_WHATSAPP_NUMBER = "whatsapp:+12186950942"; // Twilio Sandbox WhatsApp number
         String TO_WHATSAPP_NUMBER = "whatsapp:+972508266273";
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
@@ -154,6 +155,7 @@ public class MayaAlertMonitor {
         // Print the message SID for confirmation
         System.out.println("WhatsApp Message sent with SID: " + message.getSid());
     }
+
 }
 
 
